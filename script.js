97a42f63ea3c6942f861d7b186c5209f8243006c
Akshat Singh Raghuwanshi <96365527+theelectricfan@users.noreply.github.com>
2024-04-27 22:46:51 +0530

    add computer choice function

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Hello world!");
+function getComputerChoice(){
+    compChoice=Math.floor(Math.random()*3);
+    switch(compChoice){
+        case 0: return 'Rock';
+        case 1: return 'Paper';
+        case 2: return 'Scissor';
+    }
+}
+
